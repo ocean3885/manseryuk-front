139c5dc443d43a254c672b75581d0aa604e65ae4
ocean3885 <ocean3885@gmail.com>
2024-05-29 16:21:56 +0900

    2405291621

---
+++ base/views.py
@@ -0,0 +1,27 @@
+# myapp/views.py
+from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+
+def index(request):
+    # FastAPI 서버의 URL
+    fastapi_url = "https://manseryuk-api-fsjoo.run.goorm.site/calendadata/"
+    
+    # 요청 매개변수 설정
+    params = {
+        "year": 2023,
+        "month": "1",
+        "day": "1",
+        "sl": "양력",
+        "gen": "male"
+    }
+    
+    # FastAPI로 요청 보내기
+    response = requests.get(fastapi_url, params=params)
+    
+    # 응답 데이터 처리
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'error': 'Failed to retrieve data'}, status=response.status_code)
